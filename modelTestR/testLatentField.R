e99f3d8d50d52d4425eaadfc0f41d18909e5d025
famulare <mikefamulare@gmail.com>
2019-04-13 02:03:02 +0000

    It looks like the latent field is working!  Both code and science seems to do what I want, albeit much more checking is necessary.

--- modelTestR/testLatentField.R
+++ modelTestR/testLatentField.R
@@ -146,54 +146,27 @@ for(geo in geoLevels){
 
   summary(model$inla)
 
+  # plot(model$inla$summary.lincomb.derived$mode[modelDefinition$latentFieldData$pathogen=='yam'])
+  # plot(model$inla$summary.lincomb.derived$mode[modelDefinition$latentFieldData$pathogen=='rsva'])
+  # plot(model$inla$summary.lincomb.derived$mode[modelDefinition$latentFieldData$pathogen=='h1n1pdm'])
 
-  saveModel(model)
+  # idx <- model$modeledData$pathogen=='h1n1pdm' & model$modeledData$samplingLocation=='hospital'
+  # plot(log(model$modeledData$fitted.values.mode[idx]))
 
-  model<-returnModel(db$queryList,format='model',type='latentField')
+  plot(model$inla$summary.lincomb.derived$mode[modelDefinition$latentFieldData$pathogen=='h1n1pdm'],
+       log(model$modeledData$fitted.values.mode[idx]))
 
+  # plot(exp(2.9117 + 0.1164*model$modeledData$catchment[idx] +
+  #            model$inla$summary.lincomb.derived$mode[modelDefinition$latentFieldData$pathogen=='h1n1pdm']),
+  #      model$modeledData$fitted.values.mode[idx])
 
-  head(model$modeledData)
+  saveModel(model, cloudDir = 'data')
 
-  # inputData$level <- model$modeledData %>% select(pathogen,samplingLocation) %>% interaction()
-  plotSettings + geom_line(data=model$modeledData[ model$modeledData$samplingLocation=='hospital' ,],aes(x=timeBin,y=fitted.values.mode, group = PUMA5CE, color=PUMA5CE)) + facet_wrap("pathogen")
+  # model<-returnModel(db$queryList,format='model',type='latentField')
 
+}
 
-
-  # extract latent field
-  model$inla$size.random
-
-  lines(model$inla$summary.random$timeRow_rw2$ID %% 43,model$inla$summary.random$timeRow_IID$mode+model$inla$summary.random$timeRow_rw2$mode)
-
-  dim(model$inla$summary.random$PUMA5CERow)
-  dim(model$inla$summary.random$timeRow_rw2)
-  dim(model$inla$summary.fitted.values)
-
-
-
-
-
-
-  # smooth vis
-  for(k in unique(model$modeledData$samplingLocation)){
-    for (n in unique(model$modeledData$timeRow)){
-      tmp<-list(modeledData = model$modeledData[model$modeledData$samplingLocation==k & model$modeledData$timeRow == n,])
-      ggplotSmoothMap(tmp,shp,paste(k,n))
-    }
-  }
-
-  # random effects vis
-  modeledData <- inputData %>% arrange(samplingLocation,GEOIDRow)
-  nCol <- ncol(modeledData)
-  tmp<-result$summary.random$i
-  modeledData[,nCol+1:ncol(result$summary.random$i)]<-tmp
-  names(modeledData)[nCol+1:ncol(result$summary.random$i)]<-paste('fitted.values',names(result$summary.random$i),sep='.')
-  rownames(modeledData)<-c()
-  modeledData$fitted.values.mode <- exp(modeledData$fitted.values.mode)
-
-
-
-  saveModel(model, cloudDir = 'C:/Users/mfamulare/Dropbox (IDM)/SeattleFlu-incidenceMapR/models')
-
+{
   if (geo =='GEOID'){
     for(k in unique(model$modeledData$samplingLocation)){
       for (n in unique(model$modeledData$timeRow)){
@@ -202,67 +175,6 @@ for(geo in geoLevels){
       }
     }
   }
-}
-
-
-queryIn <- list(
-  SELECT   =list(COLUMN=c('num_date','pathogen','samplingLocation','GEOID')),
-  MUTATE   =list(COLUMN=c('num_date'), AS='timeBin'),
-  GROUP_BY =list(COLUMN=c('timeBin','samplingLocation','GEOID')),
-  SUMMARIZE=list(COLUMN='pathogen', IN= 'h1n1pdm')
-)
-db <- expandDB( selectFromDB(  queryIn ) )
-head(db$observedData,20)
-
-# incidence <- db$observedData %>% group_by(GEOID,timeBin,timeRow) %>% summarize(n=sum(n),positive =sum(positive, na.rm=TRUE))
-# incidence$positive[incidence$n==0]<-NA
-# incidence
-
-inputData<-cbind(db$observedData,db2$observedData)
-
-
-# multiple likelihood
-uniqueCategories <- sort(unique(db$observedData$samplingLocation))
-numLikelihoods <- length(uniqueCategories) + 1
-
-family <- rep('poisson',numLikelihoods)  # must be list of valid families
-
-inputData<-db$observedData
-inputData$GEOIDRow <- match(inputData$GEOID,unique(inputData$GEOID))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# append data
-modeledData <- db$observedData
-nCol <- ncol(modeledData)
-modeledData[,nCol+1:ncol(result$summary.fitted.values)]<-result$summary.fitted.values
-names(modeledData)[nCol+1:ncol(result$summary.fitted.values)]<-paste('fitted.values',names(result$summary.fitted.values),sep='.')
-rownames(modeledData)<-c()
-
-
-
-
-
-
-# plot
-for(k in uniqueCategories){
-  model<-list(modeledData = modeledData[modeledData$samplingLocation==k,])
-  ggplotSmoothMap(model,shp,k)
 }
 
 ###################################
