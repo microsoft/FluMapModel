a7df2373bcd2414cd3ce33b432fc2d250e7ff7fc
famulare <mfamulare@idmod.org>
2019-04-19 15:34:24 -0700

    install local packages as part of docker image build

---
+++ install_local_packages.R
@@ -0,0 +1,9 @@
+# install_local_packages
+
+pkgs <- c('dbViewR','incidenceMapR','modelTestR','modelServR')
+
+for(pkg in pkgs){
+  roxygen2::roxygenise(pkg)
+  devtools::build_vignettes(pkg)
+  devtools::install(pkg)
+}
