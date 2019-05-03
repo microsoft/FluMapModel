67214226201a4e0ccaff46552580102f6af3963d
Clinton.Collins <ccollins@idmod.org>
2019-05-03 13:37:37 -0700

    Updated production deployment files Remove old api

--- api_service/seattle_flu/controllers/__init__.py
+++
@@ -1,6 +0,0 @@
-import glob
-import importlib
-from os.path import dirname, isfile, basename
-
-modules = glob.glob(dirname(__file__)+"/*.py")
-__all__ = [importlib.import_module('{}.{}'.format(__name__, basename(f)[:-3])) for f in modules if isfile(f) and not f.endswith('__init__.py')]
