0cc426d01bad20a2cd7599c4bfd9169e01ea8022
famulare <mikefamulare@gmail.com>
2019-05-11 01:34:24 +0000

    almost...

--- modelServR/R/saveModel.R
+++ modelServR/R/saveModel.R
@@ -96,7 +96,7 @@ getModelQueryObjectFromQuery <- function(query) {
   result$observed <- sort(query$observed)
   result$model_type <- query$model_type
   result$pathogen <- query$pathogen
-  result$spatial_domain <- query$spatialDomain
+  result$spatial_domain <- query$spatial_domain
   
   logdebug("getModelQueryObjectFromQuery result:", str(result))
   return(result)
