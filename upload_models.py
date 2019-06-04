c15e2114e61e99b668a202186e6ce10150e01e10
Clinton Collins <clinton.collins@gmail.com>
2019-06-04 14:54:05 -0700

    Enable skipping errors

--- upload_models.py
+++ upload_models.py
@@ -1,18 +1,13 @@
 import argparse
 import csv
+from logging import getLogger
 import requests
 import os
 from tqdm import tqdm
-from requests_toolbelt import MultipartEncoder
+logger = getLogger()
 
 
-def zip_upload(data, files):
-    fields = data
-    fields.update(files)
-    return MultipartEncoder(fields=fields)
-
-
-def upload_model(model, api_url, models_path, api_key):
+def upload_model(model, api_url, models_path, api_key, continue_on_error=False):
     headers = {'X-Auth': api_key}
     model_path = os.path.join(models_path, f"{model['filename']}.csv")
     rds_path = os.path.join(models_path, os.path.basename(model['rds']))
@@ -30,12 +25,14 @@ def upload_model(model, api_url, models_path, api_key):
     }
 
     r = requests.post(api_url, data=model_data, headers=headers, files=files)
-    if r.status_code != 201:
-        raise Exception("upload failed")
+    if r.status_code != 201 and not continue_on_error:
+        logger.error(f"Failed to upload: {model['filename']}")
+        raise Exception(f"upload failed of {model['filename']}: {r.status_code}: {r.content}")
+    else:
+        logger.warning(f"Failed to upload: {model['filename']}: {r.status_code}: {r.content}")
 
 
 def get_models(filename):
-    rows = []
     with open(filename) as tsvfile:
         reader = csv.DictReader(tsvfile, dialect='excel-tab')
         rows = [row for row in reader]
@@ -46,8 +43,11 @@ if __name__ == "__main__":
 
     default_model_store_path = os.path.abspath(os.path.join( os.path.os.getcwd(), 'test_model_store'))
     parser = argparse.ArgumentParser(description='Uploads trained SF Models to production')
-    parser.add_argument("--db-file", default=os.path.join(default_model_store_path, "modelDB.tsv"), help="Where the modelDB.tsv produced during training is stored")
+    parser.add_argument("--db-file", default=os.path.join(default_model_store_path, "modelDB.tsv"),
+                        help="Where the modelDB.tsv produced during training is stored")
     parser.add_argument("--model-store", default=default_model_store_path)
+    parser.add_argument("--fail-on-error",dest='onerror', default=True, action='store_false',
+                        help='Allows the procressing of files to continue even in the case of an error')
     parser.add_argument("--api-url", default="http://40.112.165.255/v1/generic_models",
                         help="URL for Seattle FLU API Incidence Mapper Model Server API")
     parser.add_argument("--api-key", help="API-KEY Allowing uploads")
@@ -58,4 +58,4 @@ if __name__ == "__main__":
     pbar = tqdm(models)
     for model in pbar:
         pbar.set_description(f"Processing {model['name']}")
-        upload_model(model, args.api_url, args.model_store, args.api_key)
+        upload_model(model, args.api_url, args.model_store, args.api_key, args.onerror)
